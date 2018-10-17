c95c484d7c7b3740929373f6ad267d62560b7350
Hyewon Cho and Meria Thomas <hyewon@>
2018-10-17 09:27:10 -0700

    Make an async request to get a list of dinosaur names.

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  let dinosaurs = [];
+  let Promise = require('es6-promise').Promise;
+  let promise = new Promise(function(resolve, reject){
+    let request = new XMLHttpRequest();
+    const countParagraph = 1;
+    const countWord = 10;
+    let url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${countParagraph}&words=${countWord}`;
+    request.onload = function() {
+      if(this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+  });
+    
+  promise.then(function(response) {
+    dinosaurs = JSON.parse(response)[0];
+    console.log(dinosaurs);
+  },  function(error) {
+    $('.showError').text(`There was an error processing your request: ${error.message}`);
+  });
+
+  let targetName = dinosaurs[Math.floor(Math.random()*dinosaurs.length)];
+  
+    
+});
